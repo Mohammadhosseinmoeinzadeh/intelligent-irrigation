2c053e332b1f7a2d0227a164a0b37efccf0b4791
mohammad hossein <61532963+Mohammadhosseinmoeinzadeh@users.noreply.github.com>
2026-01-01 22:06:10 +0330

    Update water.ino

--- water.ino
+++ water.ino
@@ -1,90 +1,102 @@
-#include <LiquidCrystal.h>   
+#include <LiquidCrystal.h>
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-int set = 50;
 
-void setup() 
-{
+int setLevel = 50;
+
+
+unsigned long lastSampleTime = 0;
+const unsigned long sampleInterval = 250;   // ms
+float sumSamples = 0;
+int sampleCount = 0;
+float waterLevel = 0;
+
+
+unsigned long lastBtnTime = 0;
+const unsigned long btnDelay = 200;
+
+void setup() {
   analogReference(DEFAULT);
-  pinMode(A0, INPUT);         
+
+  pinMode(A0, INPUT);
   pinMode(8, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-  lcd.begin(16, 2);   
 
-  for (int a = 0;a <16;a++)
-  {
-    delay(250);    
-    lcd.setCursor(4, 0);               
-    lcd.print("Loading"); 
-    lcd.setCursor(a, 1);              
-    lcd.print("#");   
-  }    
+  lcd.begin(16, 2);
+
+  for (int a = 0; a < 16; a++) {
+    lcd.setCursor(4, 0);
+    lcd.print("Loading");
+    lcd.setCursor(a, 1);
+    lcd.print("#");
+    delay(150);
+  }
+  lcd.clear();
 }
-void loop() {
-  float i ; 
-  int   YL69 ;
-  float mi;
-  float mi2=0;
-  int x = 0;
-  float z;
-    
 
-  if (digitalRead(8)==LOW)
-  {
-    digitalWrite(10,HIGH);
-    delay(30);
-    digitalWrite(10,LOW);
-    set = set+5;
-    if (set > 100){set = 0;}
-   }
-      if (digitalRead(9)==LOW)
-        {
-          digitalWrite(10,HIGH);
-          delay(30);
-          digitalWrite(10,LOW);
-          set = set-5;
-          if (set<0)
-          {
-           set = 0;
-          }
-        }
-        
-  i = analogRead(A0);                
-  YL69 = 100 - (i * 100) / 1023 ;   
-  mi = YL69 - 8;
+void loop() {
+
+  unsigned long now = millis();
+
+
+  if (digitalRead(8) == LOW && now - lastBtnTime > btnDelay) {
+    lastBtnTime = now;
+    digitalWrite(10, HIGH);
+    delay(20);
+    digitalWrite(10, LOW);
+
+    setLevel += 5;
+    if (setLevel > 100) setLevel = 100;
+  }
+
   
-  for (x=0;x<=60;x++)
-    {
-     delay(250);
-      mi2 +=mi;
+  if (digitalRead(9) == LOW && now - lastBtnTime > btnDelay) {
+    lastBtnTime = now;
+    digitalWrite(10, HIGH);
+    delay(20);
+    digitalWrite(10, LOW);
+
+    setLevel -= 5;
+    if (setLevel < 0) setLevel = 0;
+  }
+
+
+  if (now - lastSampleTime >= sampleInterval) {
+    lastSampleTime = now;
+
+    int raw = analogRead(A0);
+    float level = 100.0 - (raw * 100.0 / 1023.0) - 8.0;
+
+    if (level < 0) level = 0;
+    if (level > 100) level = 100;
+
+    sumSamples += level;
+    sampleCount++;
+
+    if (sampleCount >= 60) {   
+      waterLevel = sumSamples / sampleCount;
+      sumSamples = 0;
+      sampleCount = 0;
     }
-    
-   z = mi2/60;
-   
-   if (z > 100)
-    {
-     z = 100;
-    }
-    
-  lcd.clear();                       
-  lcd.print("Water Level=");          
-  lcd.print(z);                   
-  lcd.print("%");                    
-  lcd.setCursor(2, 1);               
-  lcd.print("SET Level= ");          
-  lcd.print(set);                   
-  lcd.print("%");          
-  delay(200);                        
- 
-  if (set > z)
-    {
-      digitalWrite(11,LOW);
-    }
-    else
-    {
-      digitalWrite(11,HIGH);
-    }
- 
+  }
+
+
+  lcd.setCursor(0, 0);
+  lcd.print("Water Level=");
+  lcd.print(waterLevel, 1);
+  lcd.print("%   ");
+
+  lcd.setCursor(0, 1);
+  lcd.print("SET Level=");
+  lcd.print(setLevel);
+  lcd.print("%   ");
+
+
+  if (setLevel > waterLevel) {
+    digitalWrite(11, LOW);
+  } else {
+    digitalWrite(11, HIGH);
+  }
 }
