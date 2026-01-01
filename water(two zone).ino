0fc836607cd76a3830ffb442a9278c08ad1071d0
mohammad hossein <61532963+Mohammadhosseinmoeinzadeh@users.noreply.github.com>
2026-01-01 22:09:44 +0330

    Update water(two zone).ino

--- water(two zone).ino
+++ water(two zone).ino
@@ -1,157 +1,139 @@
-#include <LiquidCrystal.h>   
+#include <LiquidCrystal.h>
 
-LiquidCrystal lcd(7,6,5,4,3,2);
-int set = 50;
-int set2 = 50;
-void setup(){
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+
+int setZ1 = 50;
+int setZ2 = 50;
+
+
+unsigned long lastSampleTime = 0;
+const unsigned long sampleInterval = 250;
+
+unsigned long lastBtnTime = 0;
+const unsigned long btnDelay = 200;
+
+float sumZ1 = 0, sumZ2 = 0;
+int sampleCount = 0;
+
+float levelZ1 = 0;
+float levelZ2 = 0;
+
+void setup() {
   analogReference(DEFAULT);
 
-  pinMode(A0, INPUT);  
+  pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);   // Z1 +
+  pinMode(9, INPUT_PULLUP);   // Z1 -
+  pinMode(12, INPUT_PULLUP);  // Z2 +
+  pinMode(13, INPUT_PULLUP);  // Z2 -
 
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
+  pinMode(10, OUTPUT); // buzzer / beep
+  pinMode(11, OUTPUT); // valve Z1
+  pinMode(0, OUTPUT);  // valve Z2
 
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(0, OUTPUT);
+  lcd.begin(16, 2);
 
-  lcd.begin(16, 2);   
-
-    for (int a = 0;a <16;a++)
-  {
-    delay(200);    
-    lcd.setCursor(4, 0);               
-    lcd.print("Loading"); 
-    lcd.setCursor(a, 1);              
-    lcd.print("#");   
-  } 
-
+  for (int a = 0; a < 16; a++) {
+    lcd.setCursor(4, 0);
+    lcd.print("Loading");
+    lcd.setCursor(a, 1);
+    lcd.print("#");
+    delay(150);
+  }
+  lcd.clear();
 }
-void loop(){
 
-  float i ;
-  float i2;
+void loop() {
 
-  int   YL69;
-  int   kh;
-
-  
-  float mi2=0;
-
-  
-  float mi22=0;
-
-  int x = 0;
-
-  float z;
-  float z2;
+  unsigned long now = millis();
 
 
-    if (digitalRead(8)==LOW)
-  {
-    digitalWrite(10,HIGH);
-    delay(30);
-    digitalWrite(10,LOW);
-    set = set+5;
-    if (set > 100){set = 0;}
-   }
-      if (digitalRead(9)==LOW)
-        {
-          digitalWrite(10,HIGH);
-          delay(30);
-          digitalWrite(10,LOW);
-          set = set-5;
-          if (set<0)
-          {
-           set = 0;
-          }
-        }
-  
-  if (digitalRead(12)==LOW)
-  {
-    digitalWrite(10,HIGH);
-    delay(30);
-    digitalWrite(10,LOW);
-    set2 = set2+5;
-    if (set2 > 100){set2 = 0;}
-   }
-      if (digitalRead(13)==LOW)
-        {
-          digitalWrite(10,HIGH);
-          delay(30);
-          digitalWrite(10,LOW);
-          set2 = set2-5;
-          if (set2<0)
-          {
-           set2 = 0;
-          }
-        }
-  
+  if (now - lastBtnTime > btnDelay) {
 
-  i = analogRead(A0);  
-  i2 = analogRead(A1); 
+    if (digitalRead(8) == LOW) {
+      lastBtnTime = now;
+      beep();
+      setZ1 += 5;
+      if (setZ1 > 100) setZ1 = 0;
+    }
 
-  YL69 = (100 - (i * 100) / 1023)-8 ;
-  kh = (100 - (i2 * 100) / 1023)-8 ;
+    if (digitalRead(9) == LOW) {
+      lastBtnTime = now;
+      beep();
+      setZ1 -= 5;
+      if (setZ1 < 0) setZ1 = 0;
+    }
 
-  for(x=0;x<=2;x++){
-    delay(250);
-    mi2 +=YL69;
-    mi22 +=kh;
+    if (digitalRead(12) == LOW) {
+      lastBtnTime = now;
+      beep();
+      setZ2 += 5;
+      if (setZ2 > 100) setZ2 = 0;
+    }
+
+    if (digitalRead(13) == LOW) {
+      lastBtnTime = now;
+      beep();
+      setZ2 -= 5;
+      if (setZ2 < 0) setZ2 = 0;
+    }
   }
 
-  z=mi2/2;
-  z2=mi22/2;
 
-  if (z>100){
-    z = 100;
+  if (now - lastSampleTime >= sampleInterval) {
+    lastSampleTime = now;
+
+    int raw1 = analogRead(A0);
+    int raw2 = analogRead(A1);
+
+    float v1 = 100.0 - (raw1 * 100.0 / 1023.0) - 8.0;
+    float v2 = 100.0 - (raw2 * 100.0 / 1023.0) - 8.0;
+
+    v1 = constrain(v1, 0, 100);
+    v2 = constrain(v2, 0, 100);
+
+    sumZ1 += v1;
+    sumZ2 += v2;
+    sampleCount++;
+
+    if (sampleCount >= 20) {   
+      levelZ1 = sumZ1 / sampleCount;
+      levelZ2 = sumZ2 / sampleCount;
+
+      sumZ1 = 0;
+      sumZ2 = 0;
+      sampleCount = 0;
+    }
   }
-
-if (z2>100){
-    z2 = 100;
-  }
-  
-  lcd.clear();                       
-  lcd.print("Z1=");          
-  lcd.print(z);                   
-  lcd.print("%");
-
-  lcd.print("Z2=");          
-  lcd.print(z2);                   
-  lcd.print("%");
-
-
-  lcd.setCursor(1, 1);               
-  lcd.print("SZ1=");          
-  lcd.print(set);                   
+  lcd.setCursor(0, 0);
+  lcd.print("Z1=");
+  lcd.print(levelZ1, 1);
   lcd.print("% ");
 
-  lcd.print("SZ2=");          
-  lcd.print(set2);                   
-  lcd.print("%");  
+  lcd.print("Z2=");
+  lcd.print(levelZ2, 1);
+  lcd.print("% ");
 
-  delay(200);  
+  lcd.setCursor(0, 1);
+  lcd.print("S1=");
+  lcd.print(setZ1);
+  lcd.print("% ");
 
-    if (set > z)
-    {
-      digitalWrite(11,LOW);
-    }
-    else
-    {
-      digitalWrite(11,HIGH);
-    }
+  lcd.print("S2=");
+  lcd.print(setZ2);
+  lcd.print("% ");
 
-        if (set2 > z2)
-    {
-      digitalWrite(0,LOW);
-    }
-    else
-    {
-      digitalWrite(0,HIGH);
-    }
 
+  digitalWrite(11, (setZ1 > levelZ1) ? LOW : HIGH);
+  digitalWrite(0,  (setZ2 > levelZ2) ? LOW : HIGH);
+}
+
+
+void beep() {
+  digitalWrite(10, HIGH);
+  delay(20);
+  digitalWrite(10, LOW);
 }
